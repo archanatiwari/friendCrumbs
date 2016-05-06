984be7bbb45b3c6032a759b619376aaa16ca18a3
Peeyush Patil <ppatil02@clarice.com>
2016-05-06 14:20:38 +0530

    minor QA and homepage(landing page) tau removed html and dependencies added

---
+++ www/js/apps/config.js
@@ -0,0 +1,42 @@
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider.otherwise('/login');
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partials/home.html',
+            controller: 'homeController'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/login.html',
+            controller: 'loginController'
+        })
+        .state('contactList', {
+            url: '/contactList',
+            templateUrl: 'partials/contactList.html',
+            controller: 'contactListController'
+        })
+        .state('navigation', {
+            url: '/navigation',
+            templateUrl: 'partials/navigation.html',
+            controller: 'navigationController'
+        })
+        .state('location', {
+            url: '/location',
+            templateUrl: 'partials/locationPage.html',
+            controller: 'locationController'
+        })
+        .state('newEvent', {
+            url: '/newevent',
+            templateUrl: 'partials/newEvent.html',
+            controller: 'newEventController'
+        })
+        .state('inviteeList', {
+            url: '/inviteeList',
+            templateUrl: 'partials/inviteeList.html',
+            controller: 'inviteeListController'
+        })
+
+});
